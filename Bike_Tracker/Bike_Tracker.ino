25dde230ea2aa53b42ccef68741c1dddebc9670d
OscarVanL <7999692+OscarVanL@users.noreply.github.com>
2020-04-06 01:29:34 +0100

    Updated Protocol Buffer schema to make the GPS reading optional, but still report battery life. Implemented sleeping of arduino + FONA

--- Bike_Tracker/Bike_Tracker.ino
+++ Bike_Tracker/Bike_Tracker.ino
@@ -10,9 +10,15 @@
   https://github.com/lectroidmarc/gsm-tracker/blob/master/gsm_tracker/gsm_tracker.ino
   Some sections of code were taken from the Adafruit MQTT Library examples: 
   https://github.com/adafruit/Adafruit_MQTT_Library/tree/master/examples/mqtt_fona
+  I followed this Protocol Buffer guide:
+  https://www.dfrobot.com/blog-1161.html
 
   Written specifically to work with the Adafruit FONA 3G + GPS Breakout
   ----> https://www.adafruit.com/product/2687
+
+  I modified the Adafruit FONA Library to add Network Time Sync support for the FONA 3G, which
+  was not previously supported. See https://github.com/adafruit/Adafruit_FONA/pull/115
+  I also merged this RTC helper function https://github.com/adafruit/Adafruit_FONA/pull/76
  ****************************************************/
 
 #include "Adafruit_SleepyDog.h"
@@ -20,7 +26,7 @@
 #include "Adafruit_MQTT.h"
 #include "Adafruit_MQTT_FONA.h"
 #include "MQTT_Secrets.h"
-#include "protobuf/BikeTrackerPayload.pb.h"
+#include "proto_files/BikeTrackerPayload.pb.c"
 #include "pb_common.h"
 #include "pb.h"
 #include "pb_encode.h"
@@ -54,19 +60,20 @@ Adafruit_MQTT_Subscribe sleep_sub = Adafruit_MQTT_Subscribe(&mqtt, "biketracker/
 Adafruit_MQTT_Subscribe reboot_sub = Adafruit_MQTT_Subscribe(&mqtt, "biketracker/reboot");
 
 // How long to sleep device between payloads in milliseconds
-long sleep_duration = 60000;
+unsigned long sleep_duration = 60000;
+// How long the device can be unresponsive before the watchdog resets the Arduino.
+unsigned long watchdog_duration = 8000;
 
 
 void setup() {
   // Watchdog for if microcontroller crashes/freezes
-  Watchdog.enable(8000);
+  Watchdog.enable(watchdog_duration);
   
   /// FONA 3G Documentation: https://learn.adafruit.com/adafruit-fona-3g-cellular-gps-breakout/pinouts
-  Serial.begin(115200);
-  Serial.println(MQTT_BROKER);
+  Serial.begin(9600);
   Serial.println(F("Arduino started"));
-
   
+  // Initialise Arduino pins
   pinMode(FONA_KEY, OUTPUT);
   digitalWrite(FONA_KEY, HIGH);
   pinMode(FONA_PS, INPUT);
@@ -93,6 +100,7 @@ void setup() {
     Serial.println(F("Incorrect FONA in use"));
   }
 
+  // Subscribe to relevant topics
   mqtt.subscribe(&sleep_sub);
   mqtt.subscribe(&reboot_sub);
 
@@ -101,57 +109,46 @@ void setup() {
   while (1) {
     uint8_t network_status = fona.getNetworkStatus();
     if (network_status == 1 || network_status == 5) break;
-    delay(250);
+    delay(1000);
   }
 
   Watchdog.reset();
-  
+
+  // Wait until the GPS module enables, if it never does something is wrong.
   while (1) {
     if (!fona.enableGPS(true)) {
       Serial.println(F("Failed to turn on GPS"));
-      delay(300);
+      delay(1000);
     } else {
-      Serial.println(F("GPS Enabled"));
-      int8_t stat;
-      stat = fona.GPSstatus();
-      if (stat < 0) Serial.println(F("Failed to query"));
-      if (stat == 0) Serial.println(F("GPS Status: GPS off"));
-      if (stat == 1) Serial.println(F("GPS Status: No fix"));
-      if (stat == 2) Serial.println(F("GPS Status: 2D fix"));
-      if (stat == 3) Serial.println(F("GPS Status: 3D fix"));
-  
-      // check for GPS location
-      char gpsdata[120];
-      fona.getGPS(0, gpsdata, 120);
-      Serial.println(F("Reply in format: mode,fixstatus,utctime(yyyymmddHHMMSS),latitude,longitude,altitude,speed,course,fixmode,reserved1,HDOP,PDOP,VDOP,reserved2,view_satellites,used_satellites,reserved3,C/N0max,HPA,VPA"));
-      Serial.println(gpsdata);
       break;
     }
-
   }
 }
 
+int failed_gps_retries = 5;
+bool gps_captured = false;
+int attempt = 0;
+
 void loop() {
   Watchdog.reset();
-  
   int8_t stat = fona.GPSstatus();
   
-  if (stat == 0) Serial.println(F("GPS Status: GPS off"));
-  if (stat == 1) Serial.println(F("GPS Status: No fix"));
-  if (stat == 2) Serial.println(F("GPS Status: 2D fix"));
-  if (stat == 3) Serial.println(F("GPS Status: 3D fix"));
+//  if (stat == 0) Serial.println(F("GPS Status: GPS off"));
+//  if (stat == 1) Serial.println(F("GPS Status: No fix"));
+//  if (stat == 2) Serial.println(F("GPS Status: 2D fix"));
+//  if (stat == 3) Serial.println(F("GPS Status: 3D fix"));
 
-  int failed_gps_retries = 5;
-
-  if (stat >= 2) {
-    // A 2D or 3D fix means we can report, otherwise don't report anything this time.
-    float latitude, longitude, speed_kph, heading, speed_mph, altitude;
-    int attempt = 0;
-
-    if (attempt < failed_gps_retries) {
+  if (attempt < failed_gps_retries && gps_captured == false) {
+    Serial.print(F("Attempt no. "));
+    Serial.println(attempt);
+    
+    if (stat >= 2) {
+      // A 2D or 3D fix means we can report, otherwise don't report anything this time.
+      float latitude, longitude, speed_kph, heading, speed_mph, altitude;
       Watchdog.reset();
       boolean gps_success = fona.getGPS(&latitude, &longitude, &speed_kph, &heading, &altitude);
       if (gps_success) {
+        gps_captured = true;
         // FONA Library's readRTC function has been replaced with the unmerged one from https://github.com/adafruit/Adafruit_FONA/pull/76
         uint8_t year, month, day, hr, mins, sec, tz;
         fona.readRTC(&year, &month, &day, &hr, &mins, &sec, &tz);
@@ -185,38 +182,116 @@ void loop() {
         Serial.print(F("Epoch time:"));
         Serial.println(now());
         
-
-        Serial.print(F("Publishing GPS payload"));
-        uint8_t buffer[128];
-//        //BikeTrackerPayload payload = BikeTrackerPayload_init_zero;
-//        pb_ostream_t stream = pb_ostream_from_buffer(buffer, sizeof(buffer));
-//        payload.latitude = latitude;
-//        payload.longitude = longitude;
-//        // Todo: Get Epoch
-//        payload.epoch = 0;
-//        payload.battery = fona.getBattPercent();
-//        payload.speed_kph = speed_kph;
-//        pyload.heading = heading;
-//        payload.altitude = altitude;
-//        
-//        payload_pub.publish("Todo");
+        Watchdog.reset();
         
-      } else {
-        delay(300);
-        attempt += 1;
+        Serial.print(F("Creating GPS payload"));
+        uint8_t buffer[128];
+        GpsPayload payload = GpsPayload_init_zero;
+        pb_ostream_t stream = pb_ostream_from_buffer(buffer, sizeof(buffer));
+        payload.gps_fix = true;
+        payload.latitude = latitude;
+        payload.longitude = longitude;
+        payload.epoch = now();
+        uint16_t batt_percent;
+        if (fona.getBattPercent(&batt_percent)) {
+          payload.battery = batt_percent;
+        }
+        payload.speed_kph = speed_kph;
+        payload.heading = heading;
+        payload.altitude = altitude;
+
+        // Serialise Protocol Buffer
+        bool status = pb_encode(&stream, GpsPayload_fields, &payload);
+        if (!status) {
+          Serial.println(F("ERROR: Failed to encode protocol buffer"));
+        } else {
+          Serial.print(F("Message Length: "));
+          Serial.println(stream.bytes_written);
+
+          Serial.print(F("Message: "));
+          for(int i = 0; i<stream.bytes_written; i++){
+            char tmp[16];
+            sprintf(tmp, "%02X", buffer[i]);
+            Serial.print(tmp);
+          }
+        }
+        
+        Watchdog.reset();
+        //payload_pub.publish("Todo");
+        
       }
     } else {
-      Serial.println("Failed to get GPS position 5 times in a row, skipping this GPS reading");
+      Serial.println("No GPS");
+      delay(700);
+      attempt += 1;
     }
 
-    
-    
+    Watchdog.reset();
+  } else {
+    if (attempt >= failed_gps_retries) {
+      Serial.println(F("Failed to get GPS position 5 times in a row, skipping this GPS reading"));
+    } else {
+      Serial.println(F("Sleeping until next reading"));
+    }
 
-    // Todo: Take a GPS lat/long reading, report it via MQTT, then sleep FONA and then this Arduino.
+    attempt = 0;
+    int sleepMs = sleep_device(sleep_duration);
   }
 
-  Watchdog.reset();  
-  // Wait for incoming subscriptions
+}
+
+/**
+ * @brief Sleep the arduino and FONA, try to sleep for the amount of ms specified, and return the time actually
+ * slept for.
+ * FONA will be turned back on once sleep is over
+ *
+ * @param ms Milliseconds to aim to sleep for 
+ * @return int Time actually slept for in milliseconds
+ */
+int sleep_device(unsigned long ms) {
+  Serial.print(F("Sleeping for: "));
+  Serial.println(ms);
+  Serial.flush();
+  // Set the Watchdog to allow the sleep duration.
+  Watchdog.reset();
+  Serial.println(F("Turning FONA off"));
+  FONA_power_off();
+  Serial.println(F("Sleeping Arduino"));
+  Serial.flush();
+
+  // 4000ms is the most Watchdog can sleep in one go
+  unsigned long sleepMS = 0;
+  if (ms > 4000) {
+    int sleep_num = ms / 4000;
+    Watchdog.reset();
+    for(int i=0;i<sleep_num;i++) {
+      Watchdog.reset();
+      sleepMS += Watchdog.sleep(4000);
+    }
+    unsigned long extra = ms % 4000;
+    sleepMS += Watchdog.sleep(extra);
+  } else {
+    sleepMS = Watchdog.sleep(ms);
+  }
+  
+  Watchdog.reset();
+  FONA_power_on();
+
+  Serial.print(F("Device slept for: "));
+  Serial.println(sleepMS);
+  Serial.flush();
+  Serial.println(F("Arduino + FONA awake"));
+  Serial.flush();
+  return sleepMS;
+}
+
+/**
+ * @brief Check the subscribed topics
+ * Note: Not yet implemented/working
+ *
+ * @return bool true on success, false if a connection cannot be made
+ */
+bool check_subscriptions() {
   Adafruit_MQTT_Subscribe *subscription;
   while ((subscription = mqtt.readSubscription(5000))) {
     if (subscription == &sleep_sub) {
@@ -230,10 +305,6 @@ void loop() {
       delay(500);
     }
   }
-  
-
-  delay(1000);
-
 }
 
 // Function to switch FONA on
@@ -257,10 +328,8 @@ void FONA_power_off() {
   // Check FONA is actually on by reading Power State
   if (digitalRead(FONA_PS) == HIGH) {
     Serial.println(F("Powering FONA off"));
-    while (digitalRead(FONA_PS) == HIGH) {
-      digitalWrite(FONA_KEY, LOW);
-      delay(500);
-    }
+    digitalWrite(FONA_KEY, LOW);
+    delay(1000);
     digitalWrite(FONA_KEY, HIGH);
     Serial.println(F("FONA Powered Off"));
   } else {
